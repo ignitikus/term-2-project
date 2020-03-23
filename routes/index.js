33fc7dced90f03651ab507941d94d9bfa1b3f3a4
NikolayK <nikolay.kim@codeimmersive.com>
2020-03-23 17:13:32 -0400

    adds auth check for main page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.isAuthenticated())res.redirect('/')
+  return res.render('auth/login', { title: 'Express' });
 });
 
 module.exports = router;
