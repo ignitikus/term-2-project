91440dd0a1967d8e785e26eedc73a6b38f4434eb
NikolayK <nikolay.kim@codeimmersive.com>
2020-03-24 16:41:53 -0400

    adds css, partials

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  if(req.isAuthenticated())return res.render('index', {title: 'Main page'})
+router.get('/', async(req, res, next)=> {
+  if(req.isAuthenticated()){
+    const url = `http://newsapi.org/v2/everything?q=covid&apiKey=${process.env.GOOGLE_KEY}`
+    const response = await axios.get(url)
+    return res.render('index', {title: 'Main page', data: response.data})
+  }
   return res.render('auth/login', { title: 'Express' });
 });
 
-
-router.get('/test', (req,res,next) => {
-  res.render('test')
+router.get('/logout', (req,res,next) => {
+  req.logout()
+  req.session.destroy()
+  return res.redirect('/')
 })
+
 module.exports = router;
