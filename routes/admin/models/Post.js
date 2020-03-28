9469ba953f490b726f6b3ccb96fee73e9d0a257a
NikolayK <nikolay.kim@codeimmersive.com>
2020-03-28 17:16:41 -0400

    refactors code - adds controllers

--- routes/admin/models/Post.js
+++ routes/admin/models/Post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const PostSchema = new mongoose.Schema({
    author: {type: Schema.Types.ObjectId, ref:'User'},
    authorNickname: {type: 'String', trim: true, default: ''},
-   title: {type:'String', trim: true, lowercase: true, required: true},
+   title: {type:'String', trim: true, required: true},
    visibility:{type: 'Boolean', default: true},
    picture:{type: 'String', default:'', trim: true},
    content:{type: 'String', default:'', trim:true},
