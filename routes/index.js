067b864e2749ff9892ccef93c64550196f0aa641
NikolayK <nikolay.kim@codeimmersive.com>
2020-04-01 15:15:34 -0400

    adds search function for news

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,27 @@ const Post = require('./admin/models/Post')
 const moment = require('moment')
 
 router.get('/', async(req, res, next)=> {
-  if(req.isAuthenticated()){
-    const url = `http://newsapi.org/v2/everything?q=covid&apiKey=${process.env.GOOGLE_KEY}`
-    const response = await axios.get(url)
-    const allPostsInitial = await Post.find({})
+  try {
+    if(req.isAuthenticated()){
+      const url = `http://newsapi.org/v2/${
+      req.query.searchForNews 
+      ? 'everything?q='+req.query.searchForNews
+      : 'top-headlines?country=us'
+      }&apiKey=${process.env.GOOGLE_KEY}`
+      const response = await axios.get(url)
+      const allPostsInitial = await Post.find({})
 
-    const allPosts = allPostsInitial.map((post) => {
-      post.relativeTime = moment(post.time * 1000).fromNow()
-      return post;
-    })
+      const allPosts = allPostsInitial.map((post) => {
+        post.relativeTime = moment(post.time * 1000).fromNow()
+        return post;
+      })
 
-    return res.render('index', {title: 'Main page', data: response.data, allPosts})
+      return res.render('index', {title: 'Main page', data: response.data, allPosts})
+    }
+    return res.render('auth/login');
+  } catch (error) {
+    console.log(error)
   }
-  return res.render('auth/login');
 });
 
 router.get('/logout', (req,res,next) => {
